b171ff42804d500a5506bb37b43eaa6b2909c9af
bassom29 <melaniebasso@student.purdueglobal.edu>
2023-02-02 16:55:29 -0500

    Web App that connects to google maps API to accept an address and show different map views

---
+++ Google maps api web app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IT391_Basso_M5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
